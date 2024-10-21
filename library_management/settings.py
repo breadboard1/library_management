7c9a0a6b07f326ed1947fd47c33e3fa128e35308
emonhossain151 <ehosan151@gmail.com>
2024-10-22 04:53:23 +0600

    error handling

--- library_management/settings.py
+++ library_management/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://my-bank-lyzw.onrender.com', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://my-library-0l1e.onrender.com', 'https://*.127.0.0.1']
 
 
 # Application definition
